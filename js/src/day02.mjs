c73a52b287db7fb82b6a6f36adc47b133895f7ea
Shiiyu <40838999+Shiiyu@users.noreply.github.com>
2024-07-13 01:25:56 -0400

    feat: add day 2

---
+++ js/src/day02.mjs
@@ -0,0 +1,41 @@
+import {readFileSync} from "fs";
+
+const data = readFileSync("../../inputs/day02.txt", "utf8");
+
+export function solve_a() {
+  return data.split("\n").reduce((s, l) => {
+    const line = l.split(": ");
+    const game = Number(line[0].substring(5));
+    const cubes = line[1].split(/[;,]/);
+    const powers = {"red": 12, "green": 13, "blue": 14};
+    let possible = true;
+
+    for(const cube of cubes) {
+      const c = cube.trim().split(" ");
+
+      if(powers[c[1]] < Number(c[0])) {
+        possible = false;
+
+        break;
+      }
+    }
+
+    return s + (possible * game);
+  }, 0);
+}
+export function solve_b() {
+  return data.split("\n").reduce((s, l) => {
+    const line = l.split(": ");
+    const cubes = line[1].split(/[;,]/);
+    const powers = {"red": 1, "green": 1, "blue": 1};
+
+    for(const cube of cubes) {
+      const c = cube.trim().split(" ");
+
+      if(powers[c[1]] < Number(c[0]))
+        powers[c[1]] = Number(c[0]);
+    }
+
+    return s + (powers.red * powers.green * powers.blue);
+  }, 0);
+}
